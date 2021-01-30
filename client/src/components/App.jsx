8a53ff2ce5cb1475d4c33d202d2c29c0a2d3d733
Sam Cox <sjamcox@gmail.com>
2021-01-30 01:03:44 -0800

    Write helper for fetching bundle from service, add target divs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const renderService = async (path) => {
+    const response = await axios.get(path);
+    const invokeBundle = Function(response.data);
+    invokeBundle();
+  };
+
+  useEffect(() => {
+    renderService('/photos');
+    renderService('/availability');
+    renderService('/users');
+  });
+
   return (
-    <div>Congratulations. It's an App!</div>
+    <div>
+      <div id='photos'></div>
+      <div id='availabilityApp1'></div>
+      <div id='availabilityApp2'></div>
+      <div id='users'></div>
+    </div>
   );
 };
 
